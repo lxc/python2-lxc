9541b380389e451db58cba40ac005721600a2213
Stéphane Graber <stgraber@ubuntu.com>
2014-05-05 22:38:22 -0500

    Initial release of basic python2 binding
    This is the first release of a basic out of tree python2 binding. It's
    not entirely in sync with the python3 (lacks arch_to_personality) but
    otherwise should behave identically.
    
    This binding is not officially supported by LXC upstream as part of
    1.0.x nor do we have any plan to include it for any upcoming release. It
    is just released as a convenience for those who unfortunately can't yet
    use python3.
    
    Thanks to Dwight Engen for the original patchset!
    
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+#
+# python-lxc: Python bindings for LXC
+#
+# (C) Copyright Canonical Ltd. 2012
+#
+# Authors:
+# Stephane Graber <stgraber@ubuntu.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+from distutils.core import setup, Extension
+
+module = Extension('_lxc', sources=['lxc.c'], libraries=['lxc'])
+
+setup(name='_lxc',
+      version='0.1',
+      description='LXC',
+      packages=['lxc'],
+      package_dir={'lxc': 'lxc'},
+      ext_modules=[module])
